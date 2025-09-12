4907fb6578a62e15a7808f4953c2837fccc7ca8d
Gabriela Porzuczek <gporzuczek21@gmail.com>
2025-09-12 11:26:58 +0200

    fourth send

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class BaseContact:
     @property
     def label_length(self):
         self.label = len(self.name) +len(self.last_name)
-        return {self.label}
+        return self.label
     
     def __str__(self):
         return f"Wizytówka: {self.name} {self.last_name} {self.email}\n"
@@ -20,35 +20,33 @@ class BaseContact:
         return f"Wybieram numer {self.number} i dzwonie do {self.name} {self.last_name}\n"  
 
     def __repr__ (self):
-        return f"BaseContact(first_name = {self.name} last_name = {self.last_name} number = {self.number}, email = {self.email})\n" 
+        return f"Contact(first_name = {self.name} last_name = {self.last_name} number = {self.number}, email = {self.email})\n" 
 
-class BusinessContact(BaseContact):
+class BusinessContact(BaseContact): 
     def __init__(self, job, company, work_phone, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.job = job
         self.company = company
         self.work_phone = work_phone 
- 
-    def __contakt__(self):
-        return f"Wybieram numer +48{self.work_phone} i dzwonie do {self.name} {self.last_name}\n" 
-    
-    def __repr__ (self):
-        return f"BusinessContact(first_name = {self.name}, last_name = {self.last_name}, number = {self.number}, email = {self.email}, job = {self.job}, company= {self.company} work_phone={self.work_phone})\n"
 
+    def __contact__(self):
+        return f"Wybieram numer +48 {self.work_phone} i dzwonie do {self.name} {self.last_name}\n" 
+    
 def create_contacts(typee, how_many):
     for i in range(0, how_many):
         if typee == '1':
             i = BaseContact(name=fake.first_name(), last_name=fake.last_name(), number=fake.phone_number(), email=fake.email())
-            base.append(i)        
+            base.append(i)                 
         elif typee == '2':
             i = BusinessContact(name=fake.first_name(), last_name=fake.last_name(), number=fake.phone_number(), email=fake.email(),job=fake.job(), company=fake.company(),work_phone=fake.phone_number())        
             buis.append(i)
         else:
             print("Nieprawidłowy wybór") 
-            
+    return base and buis        
 if __name__=="__main__":
-    typee = input("podaj rodzaj wizytwki poslugujac sie odpowiednia cyfrom 1.BaseContact 2.BusinessContact:" )
-    how_many = int(input("Podaj ilosc wizytowek: "))
+    typee = input("podaj rodzaj wizytwki posługujac sie odpowiednia cyfrom 1.BaseContact 2.BusinessContact:" )
+    how_many = int(input("Podaj ilość wizytowek: "))
+    
     base = []
     buis = []
 
